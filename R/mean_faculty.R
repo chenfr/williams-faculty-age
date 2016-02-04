ef31fa00e7bc68a2bbf0a4596ef3b2fa1baaa8db
chenfr <frankiezchen@yahoo.com>
2016-02-04 16:51:02 -0500

    Added mean_faculty() function

---
+++ R/mean_faculty.R
@@ -0,0 +1,22 @@
+#  This file contains the mean function that calculates the average age of
+#  all current Williams Faculty.
+
+
+mean_faculty <- function(){
+
+  yearBA <- as.numeric(as.character(facultyData$V3))
+
+  #Vector of the years at which faculty obtained their BA's
+
+  2038
+
+  # 2038 = 2016 + 22. We add the current year, 2016, plus the assumed age at
+  # which a BA's is obtained, 22, and then subtract by the average year in which
+  # Williams Faculty obtained their BA's. Thus, we obtain the estimate average
+  # age of Williams faculty.
+
+  - mean(yearBA,na.rm=TRUE)
+
+  # Takes the mean of the years in which faculty obtained their BA's
+
+}
--- R/parse.R
+++ R/parse.R
@@ -1,43 +1,41 @@
-#' This file contains the functions which parse the data for this package.
+#  This file contains the functions which parse the data for this package
 
 data(facultyData)
-#' "facultyData", stored in /data, is the dataframe which lists the names of and date at which
-#'  each Williams faculty member obtained their B.A.
-#'
-#' #' HOW-TO-OBTAIN-DATA:
-#' facultyData <- read.csv("C:/Users/Frankie/Desktop/FacultyData.txt", header=FALSE, fill=TRUE, na.string="NA");
-#' Notice fill=TRUE is set, because not all professors obtained the same number of degrees (MA, PHD, BA, ect)
-#' This inequality of columns causes issues in the dataframe, which fill=TRUE solves.
-#' "FacultyData.txt" is a local text file obtained by copy and pasting the pdf of
-#' http://web.williams.edu/admin/registrar/catalog/bulletin2013_14.pdf
-#'
+#  "facultyData", stored in /data, is the dataframe which lists the names of
+#  and date at which each Williams faculty member obtained their B.A.
+#
+#  HOW-TO-OBTAIN-DATA:
+#  facultyData <- read.csv("C:/Users/Frankie/Desktop/FacultyData.txt", header=FALSE, fill=TRUE, na.string="NA");
+#  Notice fill=TRUE is set, because not all professors obtained the same number
+#  of degrees (MA, PHD, BA, ect). This inequality of columns causes issues in
+#  the dataframe, which fill=TRUE solves.
+#  "FacultyData.txt" is a local text file obtained by copy and pasting the
+#  pdf of http://web.williams.edu/admin/registrar/catalog/bulletin2013_14.pdf
 
 yearBA <- as.numeric(as.character(facultyData$V3));
 
-#' V3 is column of the years in which the Williams faculty obtained his BA
-#'
-#' "as.numeric(as.character(facultyData$V3))" obtains only the numeric values in column V3
-#' There are some non-numeric values caused by the unequal string lengths of each row.
-#' Otherwise, this would cause issue with the "mean" function.
-#' Feel free to use "summary(yearBA)" to see some quick statistics of the age of Williams faculty.
+#  V3 is column of the years in which the Williams faculty obtained his BA.
+#  "as.numeric(as.character(facultyData$V3))" obtains only the numeric values
+#  in column V3. There are some non-numeric values caused by the unequal string
+#  lengths of each row. Otherwise, this would cause issue with the "mean" function.
+#  Feel free to use "summary(yearBA)" to see some quick statistics of the age of
+#  Williams faculty.
 
 meanyearBA <- mean(yearBA,na.rm=TRUE);
 
-#' We calculate the average year at which Williams Faculty obtain their BA's to help with the next function
-#'
+#  We calculate the average year at which Williams Faculty obtain their BA's to
+#  help with the next function.
 
 meanFacultyAge <- 2016 - meanyearBA + 22;
 
-#' Assuming that most people obtain their BA at 22, we subtract the average year at which
-#' Williams Faculty obtain their BA from the current year (2016) and add 22 in order to obtain the average
-#' age of Williams Faculty.
-#'
-#'
+#  Assuming that most people obtain their BA at 22, we subtract the average year
+#  at which Williams Faculty obtain their BA from the current year (2016) and add
+#  22 in order to obtain the average age of Williams Faculty.
 
 youngestFacultyAge <- 2016 - max(yearBA, na.rm=TRUE) + 22;
 
-#' A quick estimate of the youngest Williams faculty using similar logic.
+#  A quick estimate of the youngest Williams faculty using similar logic.
 
 oldestFacultyAge <- 2016 - min(yearBA, na.rm=TRUE) + 22
 
-#' A quick estimate of the oldest Williams faculty using similar logic.
+#  A quick estimate of the oldest Williams faculty using similar logic.
